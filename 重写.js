3401c47f044a7db5b02eb75f6815e47b0ab34ab0
knight914 <81702363+knight914@users.noreply.github.com>
2025-05-07 17:03:42 +0800

    Create 重写.js

---
+++ 重写.js
@@ -0,0 +1,3 @@
+[rewrite_remote] ^https?:\/\/youhui\.95516\.com\/newsign\/api\/daily_sign_in url script-response-body https://raw.githubusercontent.com/knight914/6666/refs/heads/main/%E5%AE%9E%E9%AA%8C1.js
+
+[MITM] hostname = youhui.95516.com
